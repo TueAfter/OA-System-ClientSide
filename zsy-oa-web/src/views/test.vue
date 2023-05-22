760118e3a205b51e6ac231e425586f76e128bfa7
张 诗颖 <3177729117@qq.com>
2023-05-22 16:54:34 +0800

    v1.1

---
+++ zsy-oa-web/src/views/test.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div>账号切换</div>
+    <button @click="zhangsan()" type="default" size="mini">张三</button>
+    <button @click="lisi()" type="default" size="mini">李四</button>
+    <button @click="zzjl()" type="default" size="mini">张总经理</button>
+    <button @click="admin()" type="default" size="mini">admin</button>
+    <div>当前token：{{ token }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+
+  data() {
+    return {
+      token: ''
+    };
+  },
+
+  created(){
+    this.token = window.localStorage.getItem('token')
+  },
+
+  methods: {
+    admin() {
+      window.localStorage.setItem('token', '');
+      let token = 'eyJhbGciOiJIUzUxMiIsInppcCI6IkdaSVAifQ.H4sIAAAAAAAAAKtWKi5NUrJScgwN8dANDXYNUtJRSq0oULIyNLMwNTOyNDUy1VEqLU4t8kwBikGYeYm5qUAtiSm5mXlKtQAk0R5pQgAAAA.u9TE_oaZ0tgBBTqKbHWetRCzqzgtMbeB41ZDMJOTEmBgSctYw2t1nER3Qo5BDVmv2i6aFXZsgJ2_mJ0ARFcQ9Q'
+      window.localStorage.setItem('token', token);
+      this.token = window.localStorage.getItem('token')
+    },
+
+    zhangsan() {
+      window.localStorage.setItem('token', '');
+      let token = 'eyJhbGciOiJIUzUxMiIsInppcCI6IkdaSVAifQ.H4sIAAAAAAAAAKtWKi5NUrJScgwN8dANDXYNUtJRSq0oULIyNLMwNTOyNDMz0FEqLU4t8kxRsjKHMPMSc1OBWqoyEvPSixPzlGoBYt-vb0UAAAA.osK0-qXb22huDZIQApS5fJ6F56b937TQUi2mGrgiMlES885RjCkFFN-DnqXPFAXtT0NPX_S8N1F5eCqL22qUmA'
+      window.localStorage.setItem('token', token);
+      this.token = window.localStorage.getItem('token')
+    },
+
+    zzjl() {
+      window.localStorage.setItem('token', '');
+      let token = 'eyJhbGciOiJIUzUxMiIsInppcCI6IkdaSVAifQ.H4sIAAAAAAAAAKtWKi5NUrJScgwN8dANDXYNUtJRSq0oULIyNDM3NjMwNLYw11EqLU4t8kxRsjKFMPMSc1OBWqqqsnKUagGHebUNQQAAAA.FcCkD6QfaP8bd_3MYsRD6Tp9wr9JSRFWIUuIdG5O7XxhIeyWfzGLp40_Lesx_kWMMaaskh_ba3NrTlj8-nP6OA'
+      window.localStorage.setItem('token', token);
+      this.token = window.localStorage.getItem('token')
+    },
+
+    lisi() {
+      window.localStorage.setItem('token', '');
+      let token = 'eyJhbGciOiJIUzUxMiIsInppcCI6IkdaSVAifQ.H4sIAAAAAAAAAKtWKi5NUrJScgwN8dANDXYNUtJRSq0oULIyNLMwtTC1NDaz1FEqLU4t8kxRsjKBMPMSc1OBWnIyizOVagHddw9eQQAAAA.37LJE0gfocDW917Nl9KlJ9CJ4WIIXhtO8ZDaPQGccTs2_mYrnpUPNpFbJjpNA-nHpI78H6VK-WNujHrSv4QOjg'
+      window.localStorage.setItem('token', token);
+      this.token = window.localStorage.getItem('token')
+    }
+  }
+}
+</script>
